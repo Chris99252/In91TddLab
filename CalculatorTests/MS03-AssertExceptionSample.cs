8b4890c1095e5eea683d553b32aa3ab1930b851d
Chris99252 <chris@ccf.org.tw>
2015-09-05 23:20:51 +0800

    commit Lab02 專案初始（上一版複製到解答）

--- CalculatorTests/MS03-AssertExceptionSample.cs
+++ CalculatorTests/MS03-AssertExceptionSample.cs
@@ -5,87 +5,83 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace In91TddLab.Tests
 {
-	/// <summary>
-	/// AssertExceptionSample 的摘要描述
-	/// </summary>
-	[TestClass]
-	public class AssertExceptionSample
-	{
-		public AssertExceptionSample()
-		{
-			//
-			// TODO:  在此加入建構函式的程式碼
-			//
-		}
+    /// <summary>
+    /// AssertExceptionSample 的摘要描述
+    /// </summary>
+    [TestClass]
+    public class AssertExceptionSample
+    {
+        public AssertExceptionSample()
+        {
+            //
+            // TODO:  在此加入建構函式的程式碼
+            //
+        }
 
-		private TestContext testContextInstance;
+        private TestContext testContextInstance;
 
-		/// <summary>
-		///取得或設定提供目前測試回合
-		///的相關資訊與功能的測試內容。
-		///</summary>
-		public TestContext TestContext
-		{
-			get
-			{
-				return testContextInstance;
-			}
-			set
-			{
-				testContextInstance = value;
-			}
-		}
+        /// <summary>
+        ///取得或設定提供目前測試回合
+        ///的相關資訊與功能的測試內容。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
 
-		#region 其他測試屬性
-		//
-		// 您可以使用下列其他屬性撰寫您的測試: 
-		//
-		// 執行該類別中第一項測試前，使用 ClassInitialize 執行程式碼
-		// [ClassInitialize()]
-		// public static void MyClassInitialize(TestContext testContext) { }
-		//
-		// 在類別中的所有測試執行後，使用 ClassCleanup 執行程式碼
-		// [ClassCleanup()]
-		// public static void MyClassCleanup() { }
-		//
-		// 在執行每一項測試之前，先使用 TestInitialize 執行程式碼 
-		// [TestInitialize()]
-		// public void MyTestInitialize() { }
-		//
-		// 在執行每一項測試之後，使用 TestCleanup 執行程式碼
-		// [TestCleanup()]
-		// public void MyTestCleanup() { }
-		//
-		#endregion
+        #region 其他測試屬性
+        //
+        // 您可以使用下列其他屬性撰寫您的測試: 
+        //
+        // 執行該類別中第一項測試前，使用 ClassInitialize 執行程式碼
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // 在類別中的所有測試執行後，使用 ClassCleanup 執行程式碼
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // 在執行每一項測試之前，先使用 TestInitialize 執行程式碼 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // 在執行每一項測試之後，使用 TestCleanup 執行程式碼
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
 
-		// Exception 一定要錯的，但在測試它有可能過，這樣不就完蛋了
-		[TestMethod()]
-		public void 驗證Exception千萬別這樣幹()
-		{
-			var expected = "Attempted to divide by zero.";
-			var actual = "";
-			try
-			{
-				var zero = 0;
-				var result = 1 / zero;
-			}
-			catch (DivideByZeroException ex)
-			{
-				actual = ex.Message;
-			}
+        [TestMethod()]
+        public void 驗證Exception千萬別這樣幹()
+        {
+            var expected = "Attempted to divide by zero.";
+            var actual = "";
+            try
+            {
+                var zero = 0;
+                var result = 1 / zero;
+            }
+            catch (DivideByZeroException ex)
+            {
+                actual = ex.Message;
+            }
 
-			Assert.AreEqual(expected, actual);
-		}
-		/// <summary>
-		/// alert handler 
-		/// </summary>
-		[ExpectedException(typeof(DivideByZeroException))]
-		[TestMethod]
-		public void 該怎麼驗證Exception()
-		{
+            Assert.AreEqual(expected, actual);
+        }
 
-			var zero = 0;
-			var result = 1 / zero;
-		}
-	}
+        [TestMethod]
+        public void 該怎麼驗證Exception()
+        {
+            //var zero = 0;
+            //var result = 1 / zero;
+            Assert.Inconclusive();
+        }
+    }
 }
