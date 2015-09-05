11b0e308e8918652fa38bcb8c5ba38753883c197
Chris99252 <chris@ccf.org.tw>
2015-09-05 22:14:04 +0800

    commit 為你的測試程式重新命名, 明確告知這個方法在測什麼, 並寫上 3A 原則.

--- CalculatorTests/MyCalculatorTests.cs
+++ CalculatorTests/MyCalculatorTests.cs
@@ -8,11 +8,16 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace In91TddLab.Tests
 {
     [TestClass()]
-    public class MyCalculatorTests
+    public class AddTest_first_1_second_2_should_be_3
     {
         [TestMethod()]
         public void AddTest()
         {
+            // Arrage
+
+            // Act
+
+            // Assert
             Assert.Fail();
        } 
     }
--- In91TddLab/MyCalculator.cs
+++ In91TddLab/MyCalculator.cs
@@ -9,6 +9,8 @@ namespace In91TddLab
     /// <summary>
     /// 第一個單元測試：加法器
     /// 1. 利用 Unit Test Generator 幫你產生 TestProject
+    /// 2. 為你的測試程式重新命名，明確告知這個方法在測什麼。快捷鍵 Ctrl +　R
+    ///    並寫上 3A 原則
     /// </summary>
     public class MyCalculator
     {
