4aa54d80d69169d62c579635569e1dddde7e16ab
Chris99252 <chris@ccf.org.tw>
2015-09-05 23:30:01 +0800

    commit lab02 AreSame 驗證兩個物件相同：測試失敗

--- CalculatorTests/MS01-AssertSample.cs
+++ CalculatorTests/MS01-AssertSample.cs
@@ -77,7 +77,11 @@ namespace In91TddLab.Tests
             };
 
             //this test will pass; when you override Equals(), AreEqual will invoke Order's Equals(), rather than Object's Equals()
-            //Assert.AreSame(expected, actual); //驗證是否為同一個物件（相同）, 等同於 Assert.IsTrue(Object.RefrenceEquals(expected,actual))
+
+            //驗證是否為同一個物件（相同）, 等同於 Assert.IsTrue(Object.RefrenceEquals(expected,actual))
+            //測試失敗
+            Assert.AreSame(expected, actual);
+
             //Assert.AreEqual(expected, actual); //驗證兩個物件是否相等（相等）, 等同於 Assert.IsTrue(Object.Equals(a,b))
         }
 
