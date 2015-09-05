d76e318edf26a2c4b8f4858123fefea4327daf3c
Chris99252 <chris@ccf.org.tw>
2015-09-05 22:02:22 +0800

    commit 利用 Unit Test Generator 幫你產生 TestProject

---
+++ CalculatorTests/MyCalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using In91TddLab;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace In91TddLab.Tests
+{
+    [TestClass()]
+    public class MyCalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+       } 
+    }
+}
--- In91TddLab/MyCalculator.cs
+++ In91TddLab/MyCalculator.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace In91TddLab
 {
+    /// <summary>
+    /// 第一個單元測試：加法器
+    /// 1. 利用 Unit Test Generator 幫你產生 TestProject
+    /// </summary>
     public class MyCalculator
     {
         public int Add(int first, int second)
