4311da8d2f3b3caf483c6cc703c4f4118f808b9c
Chris99252 <chris@ccf.org.tw>
2015-09-09 01:58:14 +0800

    commit Stub 依賴注入方法，使用 function paramenter

---
+++ RsaSecureTokenTests1/v2-AuthenticationServiceTests - manualStub - function - parameter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RsaSecureToken;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace RsaSecureToken.Tests
+{
+    [TestClass()]
+    public class AuthenticationServiceTests
+    {
+        [TestMethod()]
+        public void IsValidTest()
+        {
+            var target = new AuthenticationService();
+
+            var stubProfile = new StubProfileDao();
+
+            var stubRsaToken = new StubRsaTokenDao();
+
+            var actual = target.IsValid("joey", "91000000", stubProfile, stubRsaToken);
+
+            //always failed
+            Assert.IsTrue(actual);                       
+        }
+    }
+
+    internal class StubProfileDao : IProfileDao
+    {
+
+        public string GetPassword(string account)
+        {
+            //hard-code 模擬回傳值
+            return "91";
+        }
+    }
+
+    internal class StubRsaTokenDao : IRsaTokenDao
+    {
+        public string GetRandom(string account)
+        {
+            //hard-code 模擬回傳值
+            return "000000";
+        }
+    }
+}
